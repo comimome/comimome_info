c70f7fe2e58fb810889e603756c4ae4d22088b64
comimome <76901698+comimome@users.noreply.github.com>
2023-12-26 23:31:37 +0900

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
             alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} 
+          className="logo react" 
+          alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
